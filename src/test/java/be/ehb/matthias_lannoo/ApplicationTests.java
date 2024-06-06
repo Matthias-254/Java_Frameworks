db7793f15120970e1acef7ab577ab09af7fbf306
Matthias-254 <lannoo.matthias@hotmail.com>
2024-06-06 14:39:17 +0200

    Initial commit

---
+++ src/test/java/be/ehb/matthias_lannoo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.matthias_lannoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
