1982ef03b96c5b6ba10502c4528fdaa6784360c2
Matthias-254 <lannoo.matthias@hotmail.com>
2024-06-06 20:11:29 +0200

    html pages toegevoegd

--- src/main/java/be/ehb/matthias_lannoo/controller/NewsController.java
+++ src/main/java/be/ehb/matthias_lannoo/controller/NewsController.java
@@ -1,9 +1,35 @@
 package be.ehb.matthias_lannoo.controller;
 
+import be.ehb.matthias_lannoo.model.dao.NewsDAO;
+import be.ehb.matthias_lannoo.model.dao.ReporterDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NewsController {
 
+    private final NewsDAO newsDAO;
+    private final ReporterDAO reporterDAO;
 
+    @Autowired
+    public NewsController(NewsDAO newsDAO, ReporterDAO reporterDAO) {
+        this.newsDAO = newsDAO;
+        this.reporterDAO = reporterDAO;
+    }
+
+    @GetMapping({"/", "/index"})
+    public String getIndex(){
+        return "index";
+    }
+
+    @GetMapping("/new")
+    public String getNew(){
+        return "new";
+    }
+
+    @GetMapping("/about")
+    public String getAbout(){
+        return "about";
+    }
 }
